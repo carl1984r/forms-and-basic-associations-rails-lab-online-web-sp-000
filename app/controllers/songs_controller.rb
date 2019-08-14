d50ab2d3d873a28e4a3fdc09726aa426d0893959
Bry Cox <carl.cc@gmail.com>
2019-08-14 16:15:53 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    binding.pry
     params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
 end
